40d3bd374572e0842ef74e6366f35c07e6b6f456
Tilen Gerlica <gerlica.tilen@gmail.com>
2021-05-02 18:57:27 +0200

    add_new_command_lists

--- teatasks/cli.py
+++ teatasks/cli.py
@@ -14,9 +14,33 @@ def greet():
 
 
 @main.command()
-def createlist():
+def mergelist():
+    list_merge = questionary.text("Which lists would you like to merge?").ask()
+    click.echo(f"Merged: {list_merge}")
+
+
+@main.command()
+def namelist():
     list_name = questionary.text("What would you like to name your list?").ask()
     click.echo(f"Created: {list_name}")
+
+
+@main.command()
+def searchlist():
+    list_access = questionary.text("Which list would you like to access?").ask()
+    click.echo(f"Here you go, found it! {list_access}")
+
+
+@main.command()
+def deletelist():
+    list_delete = questionary.text("Which list would you like to delete?").ask()
+    click.echo(f"Deleted: {list_delete}")
+
+
+@main.command()
+def renamelist():
+    list_rename = questionary.text("Which list would you like to rename?").ask()
+    click.echo(f"Renamed: {list_rename}")
 
 
 if __name__ == "__main__":
