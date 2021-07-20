f1f7d73b9c4c7a27b7397f44b774a5f8d9cdd144
Bastien Boutonnet <bastien.b1@gmail.com>
2021-07-20 19:05:49 +0200

    more stuff

--- teatasks/db_api.py
+++ teatasks/db_api.py
@@ -1,22 +1,50 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine("sqlite:///data/test.db", echo=True)
-Base = declarative_base()
+from sqlalchemy import Column, Integer, MetaData, String, Table, create_engine, insert
 
 
-class Lists(Base):  # type: ignore
-    __tablename__ = "lists"
-    list_id = Column(Integer, primary_key=True)
-    list_name = Column(String)
+class DbConnector:
+    def __init__(self):
+
+        self.engine = create_engine("sqlite:///data/test.db", echo=True)
+        # self.base = declarative_base()
+
+        # class Lists(self.base):  # type: ignore
+        # __tablename__ = "lists"
+        # list_id = Column(Integer, primary_key=True)
+        # list_name = Column(String)
+
+        # class Items(self.base):  # type: ignore
+        # __tablename__ = "items"
+        # item_id = Column(Integer, primary_key=True)
+        # list_id = Column(Integer, ForeignKey("lists.list_id"))
+        # title = Column(String)
+        # content = Column(String)
+
+        # self.items = Items()
+        self.metadata = MetaData()
+        # init the db
+        self.create_db()
+
+    def create_db(self):
+        """Creates table objects and instanciates them in the db."""
+        self.lists = Table(
+            "lists",
+            self.metadata,
+            Column("list_id", Integer(), primary_key=True),
+            Column("list_name", String()),
+        )
+        self.metadata.create_all(self.engine)
+
+    # create a new todo list in db
+    def create_new_todo_list(self, list_name: str) -> None:
+        # insert a new row in the Lists Class
+
+        print(f"Inserting {list_name} into the Lists table")
+        smt = insert(self.lists).values((1, list_name))
+        print(smt)
+        connection = self.engine.connect()
+        connection.execute(smt)
 
 
-class Items(Base):  # type: ignore
-    __tablename__ = "items"
-    item_id = Column(Integer, primary_key=True)
-    list_id = Column(Integer, ForeignKey("lists.list_id"))
-    title = Column(String)
-    content = Column(String)
-
-
-Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    connector = DbConnector()
+    connector.create_new_todo_list(list_name="hello world")
